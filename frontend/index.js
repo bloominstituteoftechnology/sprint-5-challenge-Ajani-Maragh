4196bf8e8e09b9f70bac520f0930b1c0c7155c2a
Ajani Maragh <amsbstart@gmail.com>
2024-04-24 17:45:47 -0700

    fetched end points and combined learners and mentors

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,37 @@
 async function sprintChallenge5() { // Note the async keyword, in case you wish to use `await` inside sprintChallenge5
   // 👇 WORK WORK BELOW THIS LINE 👇
 
+  async function getDataFromEndPoint(endpoint) {
+    const learnersRes = await axios.get('http://localhost:3003/api/learners');
+    const learnersData = learnersRes.data;
+    console.log('this is learners data:', learnersData);
+
+    const mentorsRes = await axios.get('http://localhost:3003/api/mentors');
+    const mentorsData = mentorsRes.data;
+    console.log('This is mentors data:', mentorsData);
+
+    const learnersAndMentors = [];
+    learnersData.forEach(learner => {
+      const mentorsNames = [];
+      learner.mentors.forEach(mentorId => {
+        console.log('Current Mentor Id:', mentorId);
+        const mentor = mentorsData.find(mentor => mentor.id === mentorId);
+        console.log('found mentor:', mentor);
+        mentorsNames.push(mentor ? mentor.name : `unknown mentor ${mentorId}`);
+      })
+      const learnersAndMentorsNamesCombined = {...learner, mentors: mentorsNames};
+      learnersAndMentors.push(learnersAndMentorsNamesCombined);
+      //console.log('these are learners and mentors combined', learnersAndMentors);
+    })
+    return learnersAndMentors
+
+
+  }
+  const learnersAndMentorsData = await getDataFromEndPoint();
+  console.log('learners and mentors data:', learnersAndMentorsData)
+
+  getDataFromEndPoint()
+
   const footer = document.querySelector('footer')
   const currentYear = new Date().getFullYear()
   footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`
