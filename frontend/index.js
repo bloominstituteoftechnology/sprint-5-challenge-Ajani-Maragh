b8387dc88cf6e7511e6a0d0eab2571733aa30230
Ajani Maragh <amsbstart@gmail.com>
2024-04-29 19:39:15 -0700

    finished 15 of of 19 test

--- frontend/index.js
+++ frontend/index.js
@@ -1,56 +1,26 @@
 async function sprintChallenge5() { // Note the async keyword, in case you wish to use `await` inside sprintChallenge5
   // 👇 WORK WORK BELOW THIS LINE 👇
 
-  //async function getDataFromEndPoint(endpoint) {
-    //const learnersRes = await axios.get('http://localhost:3003/api/learners');
-    //const learnersData = learnersRes.data;
-    //console.log('this is learners data:', learnersData);
-
-    //const mentorsRes = await axios.get('http://localhost:3003/api/mentors');
-    //const mentorsData = mentorsRes.data;
-    //console.log('This is mentors data:', mentorsData);
-
-    //const learnersAndMentors = [];
-    //learnersData.forEach(learner => {
-      //const mentorsNames = [];
-      //learner.mentors.forEach(mentorId => {
-        //console.log('Current Mentor Id:', mentorId);
-        //const mentor = mentorsData.find(mentor => mentor.id === mentorId);
-        //console.log('found mentor:', mentor);
-        //mentorsNames.push(mentor ? mentor.name : `unknown mentor ${mentorId}`);
-      //})
-      //const learnersAndMentorsNamesCombined = {...learner, mentors: mentorsNames};
-      //learnersAndMentors.push(learnersAndMentorsNamesCombined);
-      //console.log('these are learners and mentors combined', learnersAndMentors);
-    //})
-    //return learnersAndMentors
-
-
-  //}
-  //const learnersAndMentorsData = await getDataFromEndPoint();
-  //console.log('learners and mentors data:', learnersAndMentorsData)
-
-  //getDataFromEndPoint()
-
-
-  async function getDataFromEndPoints(endpoint) {
+    async function getDataFromEndPoints(endpoint) {
     console.log(`getting data from end point: ${endpoint}...`);
     const response = await axios.get(endpoint);
     const data = response.data;
-    console.log(`Data From ${endpoint}:`, data);
+   
     return data;
   }
+  const learnerInfo = document.querySelector('.info')
+  learnerInfo.textContent = 'No learner selected'
 
   const learnersEndPoint = 'http://localhost:3003/api/learners';
   const mentorsEndPoint = 'http://localhost:3003/api/mentors';
-
+  
   const [learnersData, mentorsData] = await Promise.all([
     getDataFromEndPoints(learnersEndPoint),
     getDataFromEndPoints(mentorsEndPoint)
   ]);
   
 console.log(learnersData, mentorsData)
-  console.log('combining learners and mentors data');
+  
   const combinedData = [];
   learnersData.forEach(learner => {
     const mentorNames = [];
@@ -65,7 +35,7 @@ console.log(learnersData, mentorsData)
     combinedData.push({ ...learner, mentors: mentorNames });
   })
 
-  console.log('combined data:', combinedData);
+  
 
   const cards = document.querySelector('.cards');
   combinedData.forEach(learner => {
@@ -75,20 +45,20 @@ console.log(learnersData, mentorsData)
       
       const learnerName = document.createElement('h3');
       learnerName.textContent = learner.fullName;
-      cards.appendChild(learnerName);
+      learnerCard.appendChild(learnerName);
       
       const learnerEmail = document.createElement('div');
       learnerEmail.textContent = learner.email;
-      cards.appendChild(learnerEmail);
+      learnerCard.appendChild(learnerEmail);
 
       const mentors = document.createElement('h4');
       mentors.classList.add('closed');
       mentors.textContent = 'Mentors';
-      cards.appendChild(mentors);
+      learnerCard.appendChild(mentors);
 
       
       const ul = document.createElement('ul');
-      cards.appendChild(ul);
+      learnerCard.appendChild(ul);
       
       const li = document.createElement('li');
       li.textContent = learner.mentors[0];
@@ -97,17 +67,58 @@ console.log(learnersData, mentorsData)
       const li2 = document.createElement('li');
       li2.textContent = learner.mentors[1]
       ul.appendChild(li2)
+      
 
-      cards.addEventListener('click', (evt) => {
-       if (evt.target === 'card') {
-           evt.target.classList = 'selected'
-       }
+      learnerCard.addEventListener('click', (evt) => {
+        // change fetching learner cards to no learner selected to the selected learner is learner.fullName
+        //append the selected learner is learner.full name after learner cards h2.
+
+        //do event lstener that adds learner id and adds or toggles  selected class to card
+        
+         if (learnerCard.classList.contains('selected')) {
+          learnerCard.classList.remove('selected')
+          learnerName.textContent = `${learner.fullName}`
+          learnerInfo.textContent = 'No learner selected'
+        } else {
+          //removing selected from every card except current card
+          //loop over the cards, remove the slected class from every single card,
+          const allCards = document.querySelectorAll('.card')
+          allCards.forEach(card => {
+            card.classList.remove('selected')
+          })
+
+          learnerInfo.textContent = `The selected learner is ${learner.fullName}`
+          learnerName.textContent =  `${learner.fullName},  ID ${learner.id}`
+          learnerCard.classList.add('selected')
+
+          
+         }
+
+         mentors.addEventListener('click', (evt) => {
+          if (learnerCard.classList.contains('selected')) {
+             
+              evt.stopPropagation()
+          }
+          if (mentors.classList.contains('closed')) {
+           mentors.classList.remove('closed')
+            mentors.classList.add('open')
+          } else if (mentors.classList.contains('open')) {
+            mentors.classList.remove('open')
+            mentors.classList.add('closed')
+          } 
+
+
+         })
+
+      
       })
 })
   
   const footer = document.querySelector('footer')
   const currentYear = new Date().getFullYear()
   footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`
+
+  //return cards
 
   // 👆 WORK WORK ABOVE THIS LINE 👆
 }
